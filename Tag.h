e4adce5f7a10f3dff5eae224758a497a3a2cd677
joanaferreira0011@gmail.com <joanaferreira0011@gmail.com>
2019-02-28 18:36:15 +0000

    fixed error

--- Tag.h
+++ Tag.h
@@ -1,4 +1,10 @@
+#ifndef TAG_H
+#define TAG_H
+
+#include <unordered_set>
+#include <string>
 typedef unordered_set<Image*> imgHashTab;
+
 
 class Tag{
   string name;
@@ -7,3 +13,5 @@ public:
   Tag(string name){this.name= name;}
   void addImage(Image* img){images.insert(img);}
 }
+
+#endif
